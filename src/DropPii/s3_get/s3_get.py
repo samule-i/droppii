1ef0afbc68f5e724634c219e6d2bb0edfe3f2785
Samuel Massey <s.massey@protonmail.com>
2024-05-23 22:40:33 +0100

    feat: add docstrings for functions

--- src/DropPii/s3_get/s3_get.py
+++ src/DropPii/s3_get/s3_get.py
@@ -2,6 +2,7 @@ import boto3
 
 
 def s3_get(s3_uri: str) -> bytes:
+    '''downloads a file from an S3 uri and returns the contents.'''
     s3_parts: list[str] = s3_uri.replace('s3://', '').split('/')
     bucket = s3_parts.pop(0)
     file_path = '/'.join(s3_parts)

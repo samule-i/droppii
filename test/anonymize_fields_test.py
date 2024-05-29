19b8e7d143d0da1d54d69844a7b8e4c8b6836fab
Samuel <s.massey@protonmail.com>
2024-05-29 19:22:51 +0100

    fix: PEP-8 change all package name references to lowecase

--- test/anonymize_fields_test.py
+++ test/anonymize_fields_test.py
@@ -8,7 +8,7 @@ import boto3
 import pytest
 from moto import mock_aws
 
-from DropPii.DropPii import anonymize_fields
+from droppii.droppii import anonymize_fields
 
 
 @mock_aws
@@ -84,11 +84,11 @@ def test_correct_parser_called_for_csv_file(populated_s3):
         }
     """
 
-    with patch("DropPii.DropPii._csv_replace_fields") as mock:
+    with patch("droppii.droppii._csv_replace_fields") as mock:
         anonymize_fields(json_string)
         anonymize_fields(parquet_string)
     mock.assert_not_called()
 
-    with patch("DropPii.DropPii._csv_replace_fields") as mock:
+    with patch("droppii.droppii._csv_replace_fields") as mock:
         anonymize_fields(csv_string)
     mock.assert_called_once()

70df3f45f17ddc27f3a4195f5a9289fb20659064
Samuel Massey <s.massey@protonmail.com>
2024-05-31 22:50:53 +0100

    fix: Change anonymize_fields to match docs

--- src/droppii/droppii.py
+++ src/droppii/droppii.py
@@ -19,17 +19,17 @@ def replace_fields(df: pl.DataFrame, private_keys: list[str]) -> pl.DataFrame:
     return df
 
 
-def anonymize_fields(json_string: str) -> bytes:
+def hide_fields(json_string: str) -> bytes:
     '''Replaces values in a file hosted on S3 based on keys present
-    in ``pii_fields``.
+    in ``private_keys``.
     returning bytes that can be passed to ``put_object``.
 
-    ``json_string`` must contains the keys ``s3_uri`` & ``pii_fields``.
+    ``json_string`` must contains the keys ``s3_uri`` & ``private_keys``.
 
     ``s3_uri`` must be a string containing the s3 uri for the file
     to be modified, and the file MUST be a valid csv file.
 
-    ``pii_fields`` must be a list containing the keys to be replaced
+    ``private_keys`` must be a list containing the keys to be replaced
     '''
     params = json.loads(json_string)
     s3_uri: str = params['s3_uri']
--- test/hide_fields_test.py
+++ test/hide_fields_test.py
@@ -8,7 +8,7 @@ import boto3
 import pytest
 from moto import mock_aws
 
-from droppii.droppii import anonymize_fields
+from droppii.droppii import hide_fields
 
 
 @mock_aws
@@ -17,14 +17,14 @@ def test_return_value_is_compatable_with_s3():
     bucket = 'test-bucket'
     key = 'test-key.csv'
 
-    args = {"s3_uri": f's3://{bucket}/{key}', "pii_fields": []}
+    args = {"s3_uri": f's3://{bucket}/{key}', "private_keys": []}
     json_string = json.dumps(args)
 
     s3client = boto3.client(service_name='s3', region_name=region)
     s3client.create_bucket(Bucket=bucket)
     s3client.put_object(Bucket=bucket, Key=key, Body='')
 
-    file_bytes = anonymize_fields(json_string)
+    file_bytes = hide_fields(json_string)
     response = s3client.put_object(Bucket=bucket, Key=key, Body=file_bytes)
     assert len(response) > 0
 
@@ -35,8 +35,8 @@ def test_csv_returns_csv(populated_s3):
     original_data = original_file["Body"].read().decode()
     original_reader = csv.reader(StringIO(original_data))
     original_keys = [row for row in original_reader][0]
-    csv_string = '{"s3_uri": "s3://test/small.csv", "pii_fields": []}'
-    csv_result = anonymize_fields(csv_string).decode()
+    csv_string = '{"s3_uri": "s3://test/small.csv", "private_keys": []}'
+    csv_result = hide_fields(csv_string).decode()
     bytes_reader = csv.reader(StringIO(csv_result))
     bytes_keys = [row for row in bytes_reader][0]
     assert bytes_keys == original_keys
@@ -48,12 +48,12 @@ def test_processes_1mb_csv_per_minute(populated_s3):
     csv_string = """
         {
             "s3_uri": "s3://test/large.csv",
-            "pii_fields": ["name", "age", "email"]
+            "private_keys": ["name", "age", "email"]
         }
     """
 
     start = time.perf_counter()
-    anonymize_fields(csv_string)
+    hide_fields(csv_string)
     stop = time.perf_counter()
     elapsed = stop - start
     if elapsed > 60:
@@ -62,33 +62,33 @@ def test_processes_1mb_csv_per_minute(populated_s3):
 
 @mock_aws
 def test_correct_parser_called_for_csv_file(populated_s3):
-    ''' Check that the correct pii field rmover is called for CSV files.
+    ''' Check that the correct field remover is called for CSV files.
     '''
 
     csv_string = """
         {
             "s3_uri": "s3://test/small.csv",
-            "pii_fields": ["name", "age", "email"]
+            "private_keys": ["name", "age", "email"]
         }
     """
     json_string = """
         {
             "s3_uri": "s3://test/small.json",
-            "pii_fields": ["name", "age", "email"]
+            "private_keys": ["name", "age", "email"]
         }
     """
     parquet_string = """
         {
             "s3_uri": "s3://test/small.parquet",
-            "pii_fields": ["name", "age", "email"]
+            "private_keys": ["name", "age", "email"]
         }
     """
 
     with patch("droppii.droppii._csv_replace_fields") as mock:
-        anonymize_fields(json_string)
-        anonymize_fields(parquet_string)
+        hide_fields(json_string)
+        hide_fields(parquet_string)
     mock.assert_not_called()
 
     with patch("droppii.droppii._csv_replace_fields") as mock:
-        anonymize_fields(csv_string)
+        hide_fields(csv_string)
     mock.assert_called_once()
